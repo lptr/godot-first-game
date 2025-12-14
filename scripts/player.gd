9e97a06e25eadf40272496acf40a6f5528c92321
Lóránt Pintér <lorant.pinter@gmail.com>
2025-12-14 18:18:40 +0100

    Animate!

--- scripts/player.gd
+++ scripts/player.gd
@@ -23,13 +23,22 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
+
+	# Flip the sprite
+	if direction < 0:
+		$Sprite.flip_h = false
+	elif direction > 0:
+		$Sprite.flip_h = true
+
 	if direction:
 		velocity.x = direction * SPEED
-		if direction == -1:
-			$Sprite.flip_h = false
-		else:
-			$Sprite.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	# Play animations
+	if direction == 0:
+		$Sprite.play("idle")
+	else:
+		$Sprite.play("run")
+		
 	move_and_slide()
