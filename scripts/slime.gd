41ced010d85e8ba046a6e6573f9acda42121bf75
Lóránt Pintér <lorant.pinter@gmail.com>
2025-12-14 16:37:50 +0100

    Flippin' slime

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -8,6 +8,8 @@ var direction = 1
 func _process(delta: float) -> void:
 	if $RayCastRight.is_colliding():
 		direction = -1
+		$Sprite.flip_h = true
 	if $RayCastLeft.is_colliding():
 		direction = 1
+		$Sprite.flip_h = false
 	position.x += direction * SPEED * delta
